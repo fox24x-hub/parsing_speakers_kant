4adcf48c7f3cd96256fd14de603eee7aed9e68f2
fox24x-hub <fox24x@gmail.com>
2026-02-06 06:37:04 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,6 @@
+# handlers/__init__.py
 from .speakers_brief import router as speakers_brief_router
+# from .other_handler import router as other_router
 
-dp.include_router(speakers_brief_router)
+# Экспортируем роутеры для использования в main.py
+__all__ = ['speakers_brief_router']
