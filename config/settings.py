961c4b7b8f4d2afc9845119df2d135f113626fae
fox24x-hub <fox24x@gmail.com>
2026-02-06 06:59:07 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,11 @@
-import os
-from dotenv import load_dotenv
+# config/settings.py
+from pydantic_settings import BaseSettings
 
-load_dotenv()
-
-class Settings:
-    def __init__(self) -> None:
-        self.openai_api_key: str = os.getenv("OPENAI_API_KEY", "")
-        self.bot_token: str = os.getenv("BOT_TOKEN", "")
-        self.postgres_url: str = os.getenv("RAILWAY_POSTGRES_URL", "")
-        self.youtube_key: str = os.getenv("YOUTUBE_API_KEY", "")
-
+class Settings(BaseSettings):
+    bot_token: str
+    
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 settings = Settings()
