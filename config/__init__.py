c828f676bf18667fc01cfda1464e0121e203ba93
fox24x-hub <fox24x@gmail.com>
2026-02-06 06:29:53 +0500

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,6 @@
 # config/__init__.py
 from .settings import Settings
-from .topics import TOPICS
+from .topics import KANT_TOPICS
 
-__all__ = ['Settings', 'TOPICS']
+__all__ = ['Settings', 'KANT_TOPICS']
+
